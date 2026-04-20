cd941c169e0e0337d27bc54ff4d18a57ec57959b
Patrick Story <ptedstoryv2@gmail.com>
2026-04-19 20:39:12 -0400

    improvements from field testing

--- heading_controller/heading_controller/pd_controller.py
+++ heading_controller/heading_controller/pd_controller.py
@@ -4,11 +4,11 @@ from serial_interfaces.msg import SensorData
 from std_msgs.msg import Float64, Float64MultiArray, String
 from motor_interfaces.msg import PWM
 
-SCREW_START = 1500
+SCREW_START = 1650
 SCREW_RANGE = 400
 SCREW_LEFT_CONSTANT = 1350
 
-CRAB_START = 1500
+CRAB_START = 1350
 CRAB_RANGE = 400
 CRAB_LEFT_CONSTANT = 1350
 
@@ -33,9 +33,9 @@ class HeadingController(Node):
         # CONTROL PARAMETERS
         # -----------------------------
         self.desired_yaw = 0.0
-        self.kp = 0.6
-        self.kd = 0.15
-        self.terrain = "dry_sand"
+        self.kp = 7
+        self.kd = 0.75
+        self.terrain = "wet_sand"
 
         # -----------------------------
         # STATE
@@ -99,25 +99,33 @@ class HeadingController(Node):
         # 4. PD CONTROL
         # -------------------------
         control = self.kp * error + self.kd * derivative
-
         # -------------------------
         # 5. SATURATION
         # -------------------------
-        control = min(1.0, max(0.0, control))
+        self.get_logger().info(f"Control: {control}")
 
         # -------------------------
         # 6. MOTOR MIXING
         # -------------------------
         pwm_msg = PWM()
-
+        #self.get_logger("kp=")
         if self.terrain == "wet_sand":
+            if msg.yaw > 0:
+                control = max(-150, min(0, control))
+            else:
+                control = max(0, min(250, control))
             # screw mode: both sides scaled around midpoint
+            self.get_logger().info("Screw mode processing")
             pwm_msg.left_pwm = float(SCREW_LEFT_CONSTANT)
-            pwm_msg.right_pwm = float(SCREW_START + control * SCREW_RANGE)
+            pwm_msg.right_pwm = float(SCREW_START + control)
         else:
+            if msg.yaw < 0:
+                control = max(0, min(150, control))
+            else:
+                control = max(-250, min(0, control))
             # crab mode: right side constant, left side adjusted by PD
             pwm_msg.left_pwm = float(CRAB_LEFT_CONSTANT)
-            pwm_msg.right_pwm = float(CRAB_START - control * CRAB_RANGE)
+            pwm_msg.right_pwm = float(CRAB_START + control)
             
 
         # -------------------------
@@ -128,13 +136,18 @@ class HeadingController(Node):
         # -------------------------
         # DEBUG
         # -------------------------
+        if self.terrain == "wet_sand":
+            mode="screw_mode"
+        else:
+            mode="crab_crawl_mode"
         self.get_logger().info(
             f"yaw={msg.yaw:.2f} "
             f"err={error:.2f} "
             f"ctrl={control:.2f} "
             f"dt={dt:.3f} "
-            f"mode={self.terrain}"
+            f"mode={mode}"
         )
+        self.get_logger().info(f"kp={self.kp}, kd={self.kd}")
 
 
 # -----------------------------
@@ -150,3 +163,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
