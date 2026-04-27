92e33d2a7bd93365e2c066ac93b8d2500103b73f
sbeknalkar <39450945+sbeknalkar@users.noreply.github.com>
2026-04-27 11:29:34 -0400

    Add files via upload

---
+++ terrain_classifier/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'terrain_classifier'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')),
+
+        # Install model files
+        (os.path.join('share', package_name, 'models'),
+            glob('models/*.pkl')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pi',
+    maintainer_email='pi@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'terrain_node = terrain_classifier.terrain_node:main',
+        ],
+    },
+)
